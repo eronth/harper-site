c0f5c5f91762843b7a8bdec2931c73d1d276c3f3
Nic Pereira <github@nicpereira.dev>
2025-08-19 15:45:01 -0500

    move cards again

---
+++ src/assets/mtg-cards/index.ts
@@ -0,0 +1,41 @@
+// MTG Card Image Imports
+// This file imports all MTG card images for proper bundling in GitHub Pages deployment
+
+import a25198BlightningImg from './a25-198-blightning.jpg';
+import blb316FestivalOfEmbersImg from './blb-316-festival-of-embers.jpg';
+import blb48EddymurkCrabImg from './blb-48-eddymurk-crab.jpg';
+import blb98HuskbursterSwarmImg from './blb-98-huskburster-swarm.jpg';
+import c13199MayaelTheAnimaImg from './c13-199-mayael-the-anima.jpg';
+import dft213MendicantCoreGuidelightImg from './dft-213-mendicant-core-guidelight.jpg';
+import dsk68OverlordOfTheFloodpitsImg from './dsk-68-overlord-of-the-floodpits.jpg';
+import fdn234VivienReidImg from './fdn-234-vivien-reid.jpg';
+import fdn30ArchmageOfRunesImg from './fdn-30-archmage-of-runes.jpg';
+import fdn307HighFaeTricksterImg from './fdn-307-high-fae-trickster.jpg';
+import fdn671DarksteelColossusImg from './fdn-671-darksteel-colossus.jpg';
+import fdn92RiteOfTheDragoncallerImg from './fdn-92-rite-of-the-dragoncaller.jpg';
+import jmp450DinrovaHorrorImg from './jmp-450-dinrova-horror.jpg';
+import lci116RampagingSpiketailImg from './lci-116-rampaging-spiketail.jpg';
+import neo234SatoruUmezawaImg from './neo-234-satoru-umezawa.jpg';
+import sld1623VorinclexVoiceOfHungerImg from './sld-1623-vorinclex-voice-of-hunger.jpg';
+import snc301CormelaGlamourThiefImg from './snc-301-cormela-glamour-thief.jpg';
+
+// Export as a map for easy lookup by filename
+export const mtgCardImages: { [key: string]: string } = {
+  'a25-198-blightning': a25198BlightningImg,
+  'blb-316-festival-of-embers': blb316FestivalOfEmbersImg,
+  'blb-48-eddymurk-crab': blb48EddymurkCrabImg,
+  'blb-98-huskburster-swarm': blb98HuskbursterSwarmImg,
+  'c13-199-mayael-the-anima': c13199MayaelTheAnimaImg,
+  'dft-213-mendicant-core-guidelight': dft213MendicantCoreGuidelightImg,
+  'dsk-68-overlord-of-the-floodpits': dsk68OverlordOfTheFloodpitsImg,
+  'fdn-234-vivien-reid': fdn234VivienReidImg,
+  'fdn-30-archmage-of-runes': fdn30ArchmageOfRunesImg,
+  'fdn-307-high-fae-trickster': fdn307HighFaeTricksterImg,
+  'fdn-671-darksteel-colossus': fdn671DarksteelColossusImg,
+  'fdn-92-rite-of-the-dragoncaller': fdn92RiteOfTheDragoncallerImg,
+  'jmp-450-dinrova-horror': jmp450DinrovaHorrorImg,
+  'lci-116-rampaging-spiketail': lci116RampagingSpiketailImg,
+  'neo-234-satoru-umezawa': neo234SatoruUmezawaImg,
+  'sld-1623-vorinclex-voice-of-hunger': sld1623VorinclexVoiceOfHungerImg,
+  'snc-301-cormela-glamour-thief': snc301CormelaGlamourThiefImg,
+};
