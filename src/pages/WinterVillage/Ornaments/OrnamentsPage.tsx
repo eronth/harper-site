8a1a18f6d9538aba1c5e81606256047d53777e7a
Nic Pereira <github@nicpereira.dev>
2025-12-26 17:47:55 -0600

    All the gay birds.

--- src/pages/WinterVillage/Ornaments/OrnamentsPage.tsx
+++ src/pages/WinterVillage/Ornaments/OrnamentsPage.tsx
@@ -6,11 +6,17 @@ import './OrnamentsPage.css';
 // Import all gaybird images
 import abigail from '../../../assets/gaybirds/Abigail.jpg';
 import cameron from '../../../assets/gaybirds/Cameron.jpg';
-import janey from '../../../assets/gaybirds/Janey.jpg';
+import janey from '../../../assets/gaybirds/Janey.webp';
 import joe from '../../../assets/gaybirds/Jo-e.jpg';
 import jodie from '../../../assets/gaybirds/Jodie.jpg';
 import peter from '../../../assets/gaybirds/Peter.jpg';
 import simon from '../../../assets/gaybirds/Simon.jpg';
+import nameless1 from '../../../assets/gaybirds/1.jpg';
+import nameless2 from '../../../assets/gaybirds/2.jpg';
+import nameless3 from '../../../assets/gaybirds/3.jpg';
+import nameless4 from '../../../assets/gaybirds/4.jpg';
+import nameless5 from '../../../assets/gaybirds/5.jpg';
+import nameless6 from '../../../assets/gaybirds/6.jpg';
 
 const gaybirdImages = [
   { src: abigail, alt: 'Abigail' },
@@ -20,6 +26,12 @@ const gaybirdImages = [
   { src: jodie, alt: 'Jodie' },
   { src: peter, alt: 'Peter' },
   { src: simon, alt: 'Simon' },
+  { src: nameless1, alt: 'Nameless Bird 1' },
+  { src: nameless2, alt: 'Nameless Bird 2' },
+  { src: nameless3, alt: 'Nameless Bird 3' },
+  { src: nameless4, alt: 'Nameless Bird 4' },
+  { src: nameless5, alt: 'Nameless Bird 5' },
+  { src: nameless6, alt: 'Nameless Bird 6' },
 ];
 
 const OrnamentsPage: React.FC = () => {
