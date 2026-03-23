1d746a44546a6b9525ee2e82f811c0dc4e8accd0
Nic Pereira <github@nicpereira.dev>
2026-03-23 05:40:58 -0500

    Cardz

--- src/pages/MagicTheGathering/MTG Data/Just Kinda Out/mtg-deck-out-data.tsx
+++ src/pages/MagicTheGathering/MTG Data/Just Kinda Out/mtg-deck-out-data.tsx
@@ -1,32 +1,32 @@
 import type { MtgDeck } from "../../../../types/mtg-types";
-import { mtgCard } from "../../mtgHelperFunctions";
+//import { mtgCard } from "../../mtgHelperFunctions";
 
 
 const justKindaOut: MtgDeck[] = [
   
-  { /* Riveting Strike */
-    id: 'riveting-strike',
-    name: 'Riveting Strike',
-    colors: ['B', 'R', 'G'],
-    shortDescription: 'A deck that focuses on high-power creatures, with good first-strike.',
-    longDescription: <>
-      Get some high power creatures on the field and grant them first strike before using Mr. Orfeo
-      to double their power, making the threat even bigger.
-    </>,
-    deckType: 'Commander',
-    commander: mtgCard(
-      'Mr. Orfeo, the Boulder', '2BG',
-      'https://scryfall.com/card/snc/204/mr-orfeo-the-boulder'
-    ),
-    keyCards: [],
-    owner: 'Nic',
-    location: {
-      case: 'Where did this go?',
-      deckbox: 'Where is this hidding?',
-      sleeves: 'Forged Planet',
-    },
-    status: 'Good',
-  },
+  // { /* Riveting Strike */
+  //   id: 'riveting-strike',
+  //   name: 'Riveting Strike',
+  //   colors: ['B', 'R', 'G'],
+  //   shortDescription: 'A deck that focuses on high-power creatures, with good first-strike.',
+  //   longDescription: <>
+  //     Get some high power creatures on the field and grant them first strike before using Mr. Orfeo
+  //     to double their power, making the threat even bigger.
+  //   </>,
+  //   deckType: 'Commander',
+  //   commander: mtgCard(
+  //     'Mr. Orfeo, the Boulder', '2BG',
+  //     'https://scryfall.com/card/snc/320/mr-orfeo-the-boulder'
+  //   ),
+  //   keyCards: [],
+  //   owner: 'Nic',
+  //   location: {
+  //     case: 'Where did this go?',
+  //     deckbox: 'Where is this hidding?',
+  //     sleeves: 'Forged Planet',
+  //   },
+  //   status: 'Good',
+  // },
   
 ];
 
